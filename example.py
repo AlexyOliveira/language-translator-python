273e599caae268c14b00771e839c66138f8e0805
AlexyOliveira <cupofteaoficial@gmail.com>
2023-08-14 22:33:33 -0300

    req 1, 2, 3 done

--- example.py
+++ example.py
@@ -3,4 +3,4 @@ from deep_translator import GoogleTranslator
 translated = GoogleTranslator(source="auto", target="pt").translate(
     "Hi, would you like some ice cream?"
 )
-print(translated) 
+print(translated)
--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,17 +1,22 @@
-# from .abstract_model import AbstractModel
-# from database.db import db
+from .abstract_model import AbstractModel
+from database.db import db
 
 
 # Req. 1
-class LanguageModel:
-    def __init__(self):
-        raise NotImplementedError
+class LanguageModel(AbstractModel):
+    _collection = db["languages"]
+
+    def __init__(self, data):
+        super().__init__(data)
 
     # Req. 2
     def to_dict(self):
-        raise NotImplementedError
+        return {
+            "name": self.data.get("name"),
+            "acronym": self.data.get("acronym"),
+        }
 
     # Req. 3
     @classmethod
     def list_dicts(cls):
-        raise NotImplementedError
+        return [cls(data).to_dict() for data in cls._collection.find()]
